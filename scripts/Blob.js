3eee81e402a67399f63b78008a05133a06a833fa
Sebastián Gaviria <sebastiangaviria@outlook.com>
2020-04-02 19:44:05 -0500

    blobs con máscara

---
+++ scripts/Blob.js
@@ -0,0 +1,92 @@
+class Blob {
+    
+  constructor (p, x, y, size){
+    this.p = p;
+    this.pos = p.createVector(x, y);
+    this.size = size;
+
+    this.col = p.color(255, 255, 0);
+
+    this.leftOffset = p.random(4125123125123);
+    this.rightOffset = p.random(4125123125123);
+    this.genOffset = p.random(4125123125123);
+  }
+
+  draw(){
+    var { p, pos, size } = this;
+
+    var mov = 40;
+    this.genOffset += 0.01;
+    pos = pos.copy().add(p.noise(this.genOffset + 45145) * mov - mov/2, p.noise(this.genOffset + 249999) * mov - mov/2);
+    size = size + p.noise(this.genOffset + 65135135) * 150;
+
+    // izquierda
+    this.leftOffset += 0.01;
+    var left = this.getSideVars(this.leftOffset, -.5, pos, size);
+    
+    // derecha
+    this.rightOffset += 0.01;
+    var right = this.getSideVars(this.rightOffset, .5, pos, size);
+
+    p.fill(this.col, 0, 0);
+    //p.stroke(0);
+    //p.strokeWeight(4);
+    p.noStroke();
+
+    // mitad abajo
+    p.bezier(left.center.x, left.center.y,
+              left.bottom.x, left.bottom.y,
+              right.bottom.x, right.bottom.y,
+              right.center.x, right.center.y);
+
+    left.center.y++;
+    left.top.y++;
+    right.center.y++;
+    right.top.y++;
+    // mitad arriba
+    p.bezier(left.center.x, left.center.y,
+              left.top.x, left.top.y,
+              right.top.x, right.top.y,
+              right.center.x, right.center.y);
+    
+    return
+    p.fill(255);
+    [left, right].forEach(function (side) {
+      ['top', 'bottom', 'center'].forEach(function (key){
+        p.ellipse(side[key].x, side[key].y, 20, 20);
+      })
+    })
+  }
+
+  getSideVars(offset, mov, pos, size){
+    var { p } = this;
+
+    var rot = p.noise(offset) * p.PI / 4 - p.PI / 8;
+
+    var posMov = 10;
+    pos = pos.copy().add(p.noise(offset + 2532) * posMov - posMov/2, p.noise(offset + 25231245123) * posMov - posMov/2);
+
+    var bottom = p.createVector(0, size * .50 + p.noise(offset + 65125265) * 100);
+    bottom.rotate(rot);
+    bottom.add(pos);
+    bottom.add(size * mov, 0);
+
+    var top = p.createVector(0, size * .5 + p.noise(offset + 211512) * 100);
+    top.rotate(rot + p.PI);
+    top.add(pos);
+    top.add(size * mov, 0);
+    
+    var center = p.createVector(pos.x - size * mov * -1, pos.y);
+
+    return {
+      top: top,
+      center: center,
+      bottom: bottom
+    }
+  }
+
+  move(x, y){
+    this.pos.set(x, y);
+  }
+
+}
--- scripts/index.js
+++ scripts/index.js
@@ -1,82 +1,41 @@
 (function() {
 
-  class Blob {
-    
-    constructor (p, x, y, size){
-      this.p = p;
-      this.pos = p.createVector(x, y);
-      this.size = size;
-
-      this.leftOffset = 0;
-    }
-
-    draw(){
-      var { p, pos, size } = this;
-
-      this.leftOffset += 0.01;
-      var rotLeft = p.noise(this.leftOffset) * p.PI / 4 - p.PI / 8;
-      //console.log(rotLeft, );
-
-      var leftBottom = p.createVector(0, size * .50 + p.noise(this.leftOffset + 600) * 10);
-      leftBottom.rotate(rotLeft);
-      leftBottom.add(pos);
-      leftBottom.add(size * -.5, 0);
-
-      var leftTop = p.createVector(0, size * .75);
-      leftTop.rotate(rotLeft + p.PI);
-      leftTop.add(pos);
-      leftTop.add(size * -.5, 0);
-
-      var pts = [
-        p.createVector(pos.x - size / 2, pos.y), // punto izquierda
-        leftBottom, // manija izquierda abajo // leftBottom
-        p.createVector(pos.x + size / 2, pos.y + size * .75), // manija derecha abajo
-        p.createVector(pos.x + size / 2, pos.y), // punto derecha
-
-        leftTop, // manija izquierda arriba
-        p.createVector(pos.x + size / 2, pos.y - size * .75), // manija derecha arriba
-      ];
-  
-      p.fill(255, 0, 0);
-      p.stroke(0);
-      p.strokeWeight(4);
-      p.bezier(pts[0].x, pts[0].y,
-             pts[1].x, pts[1].y,
-             pts[2].x, pts[2].y,
-             pts[3].x, pts[3].y);
-
-      p.bezier(pts[0].x, pts[0].y,
-              pts[4].x, pts[4].y,
-              pts[5].x, pts[5].y,
-              pts[3].x, pts[3].y);
-      
-      p.noStroke();
-      p.fill(255);
-      for (let pt of pts) {
-        //p.ellipse(pt.x, pt.y, 20, 20);
-      }    
-    }
-
-    move(x, y){
-      this.pos.set(x, y);
-    }
-
-  }
-
   new p5(function(p){
     
-    var b1 = new Blob(p, 500, 500, 200);
+    var blobs;
+    var blobGraph;
+    var bg;
+
+    p.preload = function () {
+      bg = p.loadImage('./images/gato.jpg');
+    }
     
     p.setup = function () {
       var canvas = p.createCanvas(p.windowWidth, p.windowHeight);
       canvas.parent('p5parent');
+
+      blobGraph = p.createGraphics(p.windowWidth, p.windowHeight);      
+
+      blobs = [
+        new Blob(blobGraph, 200, 150, 800),
+        new Blob(blobGraph, p.windowWidth * .4, p.windowHeight / 2, 700),
+        new Blob(blobGraph, p.windowWidth * .3, p.windowHeight - 100, 650),
+      ];
     }
 
     p.draw = function () {
-      p.background(220);
+      p.clear();
       
-      b1.draw();
+      blobGraph.clear();
+      blobs.forEach(function (blob) {
+        blob.draw();
+      });
 
+      //p.image(blobGraph, 0, 0);
+      var copy = p.createImage(p.windowWidth, p.windowHeight);
+      copy.copy(bg, 0, 0, bg.width, bg.height, 0, 0, p.windowWidth, p.windowHeight);
+      copy.mask(blobGraph);
+      p.image(copy, 0, 0);
 
       //b1.move(p.mouseX, p.mouseY);
     }
